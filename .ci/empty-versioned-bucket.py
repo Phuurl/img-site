016e72b7e829ed02c9e708c191265269057428d1
Phil Corbett <1835431+Phuurl@users.noreply.github.com>
2022-04-23 20:16:15 +0100

    Adds initial PR deploy workflow (#3)
    * Adds initial PR deploy workflow
    
    * Corrects if event statement
    
    * Updates `sed` syntax to GNU version
    
    * You must construct additional `\`s
    
    * Switches to non-container build
    
    * Updates tags and adds test input image
    
    * Corrects CF output query strings
    
    * Corrects output query quotes again
    
    * Adds naming for CloudFront redirect Function
    
    * Applies consistent `PR` capitalisation
    
    * Adds PR cleanup workflow (#4)
    
    * Adds PR cleanup workflow
    
    * Adds `CloudFrontCachePolicyName`
    
    * Corrects cleanup workflow (#5)
    
    * Adds required SAM stack args (#6)
    
    * Removes unsupported `--role-arn` from SAM destroy (#7)
    
    * Removes `sam delete` confirmation requirement (#8)
    
    * Adds invalid arg exit to Python script

---
+++ .ci/empty-versioned-bucket.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+
+import boto3
+
+if len(sys.argv) != 2:
+    print("Missing / invalid arguments\nRequires bucket name as 1st and only script argument.")
+    sys.exit(1)
+bucket_name = sys.argv[1]
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(bucket_name)
+bucket.object_versions.delete()
